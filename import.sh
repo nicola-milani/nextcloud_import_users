8f9d38ebb663c09af4bd5ffb63a6a3ac5148df53
Nicola Milani <nicoweb313@gmail.com>
2019-06-17 04:33:38 +0200

    order options in csv

--- import.sh
+++ import.sh
@@ -2,7 +2,7 @@
 #Command line tool for import from csv
 
 function check_req(){
-
+    
     command -v pwgen >> /dev/null
     if [ $? = 1 ]; then
         echo "please install pwgen"
@@ -24,57 +24,55 @@ function do_run(){
     var_apache_user=${webuser}
     var_path_nextcloud=${nxt_path}
     var_result_file="${log_path}/${var_datum}_user_create.txt"
-
+    
     i=1
-#set -x
-while read -r line 
-do
-
+    #set -x
+    while read -r line
+    do
+        
         [ -z "$line" ] && continue
         test $i -eq 1 && ((i=i+1)) && continue
-
-    #   echo "Rang: ${line}"
+        
+        #   echo "Rang: ${line}"
         var_password=$(pwgen 8 -c -n -N 1)
         set -e
         export OC_PASS=$var_password
-    #   echo "${var_password} ${OC_PASS}"
-        var_username=$(echo "${line}" | cut -d"," -f2)
+        #   echo "${var_password} ${OC_PASS}"
         var_name=$(echo "${line}" | cut -d"," -f1)
-        var_group1=$(echo "${line}" | cut -d"," -f3)
-        var_group2=$(echo "${line}" | cut -d"," -f4)
-        var_group3=$(echo "${line}" | cut -d"," -f5)
-        var_group4=$(echo "${line}" | cut -d"," -f6)
-        var_group5=$(echo "${line}" | cut -d"," -f7)
-        var_email=$(echo "${line}" | cut -d"," -f8)
-        var_quota=$(echo "${line}" | cut -d"," -f9)
-	var_group1="prova"
-
+        var_username=$(echo "${line}" | cut -d"," -f2)
+        var_email=$(echo "${line}" | cut -d"," -f3)
+        var_quota=$(echo "${line}" | cut -d"," -f4)
+        var_group1=$(echo "${line}" | cut -d"," -f5)
+        var_group2=$(echo "${line}" | cut -d"," -f6)
+        var_group3=$(echo "${line}" | cut -d"," -f7)
+        var_group4=$(echo "${line}" | cut -d"," -f8)
+        var_group5=$(echo "${line}" | cut -d"," -f9)
         groups=""
-	if [ ! -z "${var_group1}" ]; then
-		group=$group" --group"${var_group1}
-	fi
-	if [ ! -z "${var_group2}" ]; then
-		group=$group" --group"${var_group2}
-	fi
-	if [ ! -z "${var_group3}" ]; then
-		group=$group" --group"${var_group3}
-	fi
-	if [ ! -z "${var_group4}" ]; then
-		group=$group" --group"${var_group4}
-	fi
-	if [ ! -z "${var_group5}" ]; then
-		group=$group" --group"${var_group5}
-	fi
-  	if [ ! -z "${group}" ]; then
-        	su -s /bin/bash ${var_apache_user} -c "php ${var_path_nextcloud}/occ user:add ${var_username} --password-from-env ${groups} --display-name='${var_name}'"
+        if [ ! -z "${var_group1}" ]; then
+            group=$group" --group"${var_group1}
+        fi
+        if [ ! -z "${var_group2}" ]; then
+            group=$group" --group"${var_group2}
+        fi
+        if [ ! -z "${var_group3}" ]; then
+            group=$group" --group"${var_group3}
+        fi
+        if [ ! -z "${var_group4}" ]; then
+            group=$group" --group"${var_group4}
+        fi
+        if [ ! -z "${var_group5}" ]; then
+            group=$group" --group"${var_group5}
+        fi
+        if [ ! -z "${group}" ]; then
+            su -s /bin/bash ${var_apache_user} -c "php ${var_path_nextcloud}/occ user:add ${var_username} --password-from-env ${groups} --display-name='${var_name}'"
         else
-                su -s /bin/bash ${var_apache_user} -c "php ${var_path_nextcloud}/occ user:add ${var_username} --password-from-env --display-name='${var_name}'"
-	fi
-	su -s /bin/bash ${var_apache_user} -c " php ${var_path_nextcloud}/occ user:setting ${var_username} settings email '${var_email}'"
+            su -s /bin/bash ${var_apache_user} -c "php ${var_path_nextcloud}/occ user:add ${var_username} --password-from-env --display-name='${var_name}'"
+        fi
+        su -s /bin/bash ${var_apache_user} -c " php ${var_path_nextcloud}/occ user:setting ${var_username} settings email '${var_email}'"
         su -s /bin/bash ${var_apache_user} -c " php ${var_path_nextcloud}/occ user:setting ${var_username} files quota '${var_quota}'"
         echo "${var_username};${var_password}" >> "${var_result_file}"
-done < "$input"
-exit 0
+    done < "$input"
+    exit 0
 }
 
 function do_help(){
@@ -83,32 +81,33 @@ function do_help(){
 $0 --csv /path/of/users_csv --header [true|false] --sep ";" --debug [true|false] --nxt_path /path/nextcloud --log_path /path/output/log
 --help: show this
 --csv: path of csv with users
---sep: separator for csv ex ; or , etc.. 
+--sep: separator for csv ex ; or , etc..
 --nxt_path: absolute path of nextcloud installation
 --log_path: path for username and password output
 --debug: enable verbose output
 --default: print default value
 --get_example: get csv example structure with number of user groups
 EOF
-
+    
 }
 
 
 function get_example(){
-
-if [ -f $HOME/users_template.csv ]; then
-    echo "file  $HOME/users_template.csv already exist"
-    exit 1
-fi
-cat <<EOT >> ~/users_template.csv
-Nome e Cognome,username,group1,group2,group3,group4,group5,E-mail,Quota
+    
+    if [ -f $(pwd)/users_template.csv ]; then
+        echo "file  $(pwd)/users_template.csv already exist"
+        exit 1
+    fi
+cat <<EOT >> $(pwd)/users_template.csv
+Name,username,email,quota,group1,group2,group3,group4,group5
+Mario Rossi,m.rossi,m.rossi@example.com,2GB,,,,,
 
 EOT
-
+    
     if [ $? = 1 ]; then
         echo "chek permissions"
     else
-        echo "example created in $HOME"
+        echo "example created in $(pwd)"
     fi
 }
 function print_default(){
@@ -119,10 +118,10 @@ function print_default(){
 function print_value(){
     echo -e "csv path: $(pwd)/users.csv"
     echo -e "with header: " $header
-    echo -e "separator file: " "\""$sep"\"" 
-    echo -e "web user: "$webuser 
-    echo -e "nextcloud path: " $nxt_path 
-    echo -e "path of output:  "$log_path 
+    echo -e "separator file: " "\""$sep"\""
+    echo -e "web user: "$webuser
+    echo -e "nextcloud path: " $nxt_path
+    echo -e "path of output:  "$log_path
     echo -e "status of debug: "$debug
 }
 
@@ -134,8 +133,10 @@ debug="false"
 webuser="root"
 nxt_path="/var/www"
 log_path="/var/log/nxt_tools/"
-mkdir -p $log_path
-
+mkdir -p $log_path &> /dev/null
+if [ $? -eq 1 ]; then
+    echo "can't write in $log_path"
+fi
 function do_import() {
     clear
     echo "Running options..."
@@ -159,58 +160,58 @@ fi
 eval set -- "$ARGS"
 
 while true; do
-
-   case $1 in
-    -h|--help)
-        shift
-        do_help
-        exit 0
+    
+    case $1 in
+        -h|--help)
+            shift
+            do_help
+            exit 0
         ;;
-    --csv)
-        shift
-        csv=$1
+        --csv)
+            shift
+            csv=$1
         ;;
-    --header)
-        shift
-        header=$1
+        --header)
+            shift
+            header=$1
         ;;
-    --sep)
-        shift
-        sep=$1
+        --sep)
+            shift
+            sep=$1
         ;;
-    --webuser)
-        shift
-        webuser=$1
+        --webuser)
+            shift
+            webuser=$1
         ;;
-    --nxt_path)
-        shift
-        nxt_path=$1
+        --nxt_path)
+            shift
+            nxt_path=$1
         ;;
-    --log_path)
-        shift
-        log_path=$1
+        --log_path)
+            shift
+            log_path=$1
         ;;
-    --debug)
-        shift
-        debug="true"
-        #exit 0
+        --debug)
+            shift
+            debug="true"
+            #exit 0
         ;;
-    --default)
-        shift
-        print_default
-        exit 0
+        --default)
+            shift
+            print_default
+            exit 0
         ;;
-    --get_example)
-        shift
-        get_example
-        exit 0
+        --get_example)
+            shift
+            get_example
+            exit 0
         ;;
-    *)
-        shift
-        break
+        *)
+            shift
+            break
         ;;
-   esac
-   shift
+    esac
+    shift
 done
 
 do_import
